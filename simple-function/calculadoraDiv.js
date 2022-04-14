9766543f3b359d4a7c7a6b4f134530d746a215cf
beatriz.xavier <beatriz.xavier@ifood.com.br>
2022-04-14 16:52:38 -0300

    indentei a função divisão da calculadora

--- simple-function/calculadoraDiv.js
+++ simple-function/calculadoraDiv.js
@@ -1,7 +1,7 @@
-function divisão (numeroA, numeroB) {
+function divide (numeroA, numeroB) {
     let numeroC = numeroA / numeroB
     return numeroC
-    }
+}
     
-    let resultado = divisão (10,2)
-    console.log (resultado)
+let resultado = divide (10,2)
+console.log (resultado)
